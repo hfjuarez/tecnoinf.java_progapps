1a6a21e131ad3a5f589267eddde34b959ce83d68
Herfj <54822117+Herfj@users.noreply.github.com>
2020-09-02 23:05:38 -0300

    update un cursito

--- prog/src/logica/entidades/Curso.java
+++ prog/src/logica/entidades/Curso.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.lang.String;
 import java.sql.Date;
 import javax.persistence.*;
+import logica.entidades.Instituto;
+import java.util.List;
 
 /**
  * Entity implementation class for Entity: Curso
@@ -11,78 +13,107 @@ import javax.persistence.*;
  */
 @Entity
 @Table
-public class Curso {
+public class Curso  {
 
-    @Id
-    private String nombre;
-    private String desc;
-    private int duracion;
-    private int cantHoras;
-    private int cantCred;
-    private String URL;
-    private Date FechaAlta;
+	   
+	@Id
+	private String nombreCurso;
+	private String descCurso;
+	private int duracionMeses;
+	private int cantidadHoras;
+	private int cantidadCreditos;
+	private String URL;
+	private Date fechaAlta;
+	@ManyToOne
+	private Instituto instituto;
+	@OneToMany(targetEntity=Curso.class)
+	private List previas;
+	//private static final long serialVersionUID = 1L;
 
-    private Instituto instituto;
-    // private static final long serialVersionUID = 3L;
+	public Curso() {
+		super();
+	}   
+	
+	public Curso(String nombreCurso, String descCurso, int duracionMeses, int cantidadHoras, int cantidadCreditos,
+			String uRL, Date fechaAlta, Instituto instituto) {
+		super();
+		this.nombreCurso = nombreCurso;
+		this.descCurso = descCurso;
+		this.duracionMeses = duracionMeses;
+		this.cantidadHoras = cantidadHoras;
+		this.cantidadCreditos = cantidadCreditos;
+		URL = uRL;
+		this.fechaAlta = fechaAlta;
+		this.instituto = instituto;
+	}
 
-    public Curso() {
-        super();
-    }
+	public String getNombreCurso() {
+		return this.nombreCurso;
+	}
 
-    public String getNombreCurso() {
-        return this.nombre;
-    }
+	public void setNombreCurso(String nombreCurso) {
+		this.nombreCurso = nombreCurso;
+	}   
+	public String getDescCurso() {
+		return this.descCurso;
+	}
 
-    public void setNombreCurso(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setDescCurso(String descCurso) {
+		this.descCurso = descCurso;
+	}   
+	public int getDuracionMeses() {
+		return this.duracionMeses;
+	}
 
-    public String getDescripcion() {
-        return this.desc;
-    }
+	public void setDuracionMeses(int duracionMeses) {
+		this.duracionMeses = duracionMeses;
+	}   
+	public int getCantidadHoras() {
+		return this.cantidadHoras;
+	}
 
-    public void setDescripcion(String desc) {
-        this.desc = desc;
-    }
+	public void setCantidadHoras(int cantidadHoras) {
+		this.cantidadHoras = cantidadHoras;
+	}   
+	public int getCantidadCreditos() {
+		return this.cantidadCreditos;
+	}
 
-    public int getDuracion() {
-        return this.duracion;
-    }
+	public void setCantidadCreditos(int cantidadCreditos) {
+		this.cantidadCreditos = cantidadCreditos;
+	}   
+	public String getURL() {
+		return this.URL;
+	}
 
-    public void setDuracion(int duracion) {
-        this.duracion = duracion;
-    }
+	public void setURL(String URL) {
+		this.URL = URL;
+	}   
+	public Date getFechaAlta() {
+		return this.fechaAlta;
+	}
 
-    public int getCantHoras() {
-        return this.cantHoras;
-    }
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}   
+	public Instituto getInstituto() {
+		return this.instituto;
+	}
 
-    public void setCantHoras(int cantHoras) {
-        this.cantHoras = cantHoras;
-    }
+	public void setInstituto(Instituto instituto) {
+		this.instituto = instituto;
+	}
 
-    public int getCantCred() {
-        return this.cantCred;
-    }
+	public List getPrevias() {
+		return previas;
+	}
 
-    public void setCantCred(int cantCred) {
-        this.cantCred = cantCred;
-    }
-
-    public String getURL() {
-        return this.URL;
-    }
-
-    public void setURL(String URL) {
-        this.URL = URL;
-    }
-
-    public Date getFecha() {
-        return this.FechaAlta;
-    }
-
-    public void setFecha(Date Fecha) {
-        this.FechaAlta = Fecha;
-    }
-
+	public void setPrevias(List previas) {
+		this.previas = previas;
+	}
+	
+	public void addPrevia(Curso cursoPrevio) {
+		this.previas.add(cursoPrevio);
+	}
+   
 }
